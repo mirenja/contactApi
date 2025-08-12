8e398d22104683a94c118afbb83ced89675d3126
mirenja <gillian.kenyani@code.berlin>
2025-08-12 17:50:39 +0200

    feat:updated terraform outputs

--- terraform-gcp-cloudfunctions/outputs.tf
+++ terraform-gcp-cloudfunctions/outputs.tf
@@ -1,3 +1,7 @@
 output "function_uri" {
   value = google_cloudfunctions2_function.default.service_config[0].uri
 }
+
+output "cloud_function_name" {
+  value = google_cloudfunctions2_function.default.name
+}
