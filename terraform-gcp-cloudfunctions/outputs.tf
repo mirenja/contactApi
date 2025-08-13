662085d8e48901280a37674bdc5e8852f0e647cd
mirenja <gillian.kenyani@code.berlin>
2025-08-13 14:38:57 +0200

    fix:sync with terraform cloud

--- terraform-gcp-cloudfunctions/outputs.tf
+++ terraform-gcp-cloudfunctions/outputs.tf
@@ -1,6 +1,6 @@
-##output "function_uri" {
-#  value = google_cloudfunctions2_function.default.service_config[0].uri
-#}
+output "function_uri" {
+  value = google_cloudfunctions2_function.default.service_config[0].uri
+}
 
 output "cloud_function_name" {
   value = google_cloudfunctions2_function.default.name
